57a03ac6da3a35781e86f9236b30d2523430664d
Rahmidin Hasanov <rahmiddin.hasanov@bk.ru>
2022-07-24 20:29:35 +0500

    2

--- INTERFACE.py
+++ INTERFACE.py
@@ -5,13 +5,13 @@ import requests
 class FlatIterator:
     def __init__(self, multi_list):
         self.multi_list = multi_list
-        
+
     def __iter__(self):
         self.list_iter = iter(self.multi_list)
         self.nested_list = []
         self.cursor = -1
         return self
-    
+
     def __next__(self):
         self.cursor += 1
         if len(self.nested_list) == self.cursor:
@@ -20,6 +20,7 @@ class FlatIterator:
             while not self.nested_list:
                 self.nested_list = next(self.list_iter)
         return self.nested_list[self.cursor]
+
 
 @multiply('C:/Users/rahmi/Documents/PycharmProjects/pythonProject/INTERFACE')
 def flat_generator(my_list):
@@ -44,3 +45,4 @@ if __name__ == '__main__':
 
     for item in flat_generator(nested_list):
         print(item)
+
--- logger.py
+++ logger.py
@@ -10,7 +10,6 @@ def loger(functions):
     
     @wraps(functions)
     def time_log(*args, **kwargs):
-        
         with open('file.text', 'w') as file:
             file.write(f'Была запушена функция {functions.__name__}' + '\n')
             file.write(f'Время и дата: {datetime.datetime.now()}' + '\n')
@@ -34,7 +33,6 @@ def multiply(url):
                 file.write(f'Время и дата: {datetime.datetime.now()}' + '\n')
                 file.write(f'Аргументы: {*args, *kwargs}' + '\n')
                 file.write(f'Возврашаемое значение: {functions(*args, **kwargs)}')
-        
+                return args, kwargs
         return time_log
-
     return loger_with_url
